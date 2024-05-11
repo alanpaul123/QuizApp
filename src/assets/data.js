e3e26f66e850b41e5ba4963504a1ac7c863945a5
alanpaul123 <alanpauldxb@gmail.com>
2024-05-11 08:49:26 +0400

    complete

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+      question: "Which of the following command is used to create react-js-app ?",
+      option1: "npx create-react-app appname",
+      option2: "npm install create-react-app",
+      option3: "npx install create-react-app -g",
+      option4: "install - l create-react-app",
+      ans: 1,
+    },
+    {
+      question: "In React.js which one of the following is used to create a class for Inheritance ?",
+      option1: "Create",
+      option2: "Extends",
+      option3: "Inherits",
+      option4: "Delete",
+      ans: 2,
+    },
+    {
+      question: "What is the default port number in which the application run ?",
+      option1: "3000",
+      option2: "8080",
+      option3: "5000",
+      option4: "3030",
+      ans: 1,
+    },
+    {
+      question: "Which of the following valid component return type of React ?",
+      option1: "2",
+      option2: "5",
+      option3: "1",
+      option4: "3",
+      ans: 3,
+    },
+    {
+      question: "Which of the following is a way to handle data in React.js ?",
+      option1: "State & Props",
+      option2: "Services & Components",
+      option3: "State & Services",
+      option4: "State & Component",
+      ans: 1,
+    },
+  ];
